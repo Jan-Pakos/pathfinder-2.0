30e9cf4c5f13f9423695c5dc01f8d18301db73b8
Jan-Pakos <janpakos123@gmail.com>
2026-03-02 17:15:24 +0100

    created inmemory db and test class

---
+++ pathfinder/src/test/java/domain/citycrud/CityCrudFacadeTest.java
@@ -0,0 +1,15 @@
+package domain.citycrud;
+
+import static org.mockito.Mockito.mock;
+
+class CityCrudFacadeTest {
+
+    CityCrudFacade facade =  mock(CityCrudFacade.class);
+
+
+    void shouldAddNode() {
+        // given
+        // when
+        // then
+    }
+}
---
+++ pathfinder/src/test/java/domain/citycrud/NodeAndConnectionInMemoryRepository.java
@@ -0,0 +1,82 @@
+package domain.citycrud;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+class NodeAndConnectionInMemoryRepository implements NodeAndConnectionRepository {
+
+    private final ConcurrentHashMap<Long, Node> db;
+    private final AtomicLong counter;
+
+    public NodeAndConnectionInMemoryRepository() {
+        this.db = new ConcurrentHashMap<>();
+        this.counter = new AtomicLong(0);
+    }
+
+    @Override
+    public <S extends Node> S save(S entity) {
+        if (entity.getId() == null) {
+            long id = counter.incrementAndGet();
+            entity.setId(id);
+        }
+
+        db.put(entity.getId(), entity);
+        return entity;
+    }
+
+    @Override
+    public <S extends Node> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Node> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<Node> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Node> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Node entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Node> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
