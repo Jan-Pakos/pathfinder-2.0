70f70ec7967826dc6fe66382f3827d552a1fc17b
Jan-Pakos <janpakos123@gmail.com>
2026-03-04 17:26:26 +0100

    fix

--- pathfinder/src/main/java/com/example/pathfinder/infrastructure/citycrud/controller/CityCrudRestController.java
+++ pathfinder/src/main/java/com/example/pathfinder/infrastructure/citycrud/controller/CityCrudRestController.java
@@ -3,7 +3,6 @@ package com.example.pathfinder.infrastructure.citycrud.controller;
 import com.example.pathfinder.domain.citycrud.CityCrudFacade;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
